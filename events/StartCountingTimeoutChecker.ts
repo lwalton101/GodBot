327f2e7cadcc50d2160b2181aec4a2f1d2c7a786
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2023-11-18 20:25:19 +0000

    new infinite timeout code

---
+++ events/StartCountingTimeoutChecker.ts
@@ -0,0 +1,28 @@
+import {config} from "../index";
+import {client} from "../discord";
+import {EventHandler} from "../EventHandler";
+import {Events, Message} from "discord.js";
+export const eventHandler: EventHandler = {
+    name: Events.ClientReady,
+    once: false,
+    execute() {
+        setInterval(async () => {
+            for(let guildId of config.getConfigOption("countingTimeoutGuilds").split(",")){
+                 let guild = await client.guilds.fetch(guildId).catch(e => {})
+                 if(!guild){
+                     return;
+                 }
+                 for(let id of config.getConfigOption("countingTimeoutMembers").split(",")){
+                    let guildMember = await guild.members.fetch(id).catch(e => {});
+                    if(!guildMember){
+                        return;
+                    }
+                    if(guildMember.communicationDisabledUntilTimestamp > Date.now()){
+                        await guildMember.timeout(guildMember.communicationDisabledUntilTimestamp - Date.now() + (Number.parseInt(config.getConfigOption("countingTimeoutCheckoutAmount")) * 2));
+                    }
+                }
+            }
+        }, config.getConfigOption("countingTimeoutCheckoutAmount"))
+    }
+
+}
