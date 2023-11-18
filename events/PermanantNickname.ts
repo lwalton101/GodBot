10a4e01579caf164394b0cae701845c55692b497
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2023-11-18 18:55:01 +0000

    updated Permanent Nickname to use new config system

--- events/PermanantNickname.ts
+++ events/PermanantNickname.ts
@@ -1,17 +1,21 @@
 import {EventHandler} from "../EventHandler";
 import {Client, Events, GuildMember} from "discord.js";
+import {config} from "../index";
 
 export const eventHandler: EventHandler = {
     name: Events.GuildMemberUpdate,
     once: false,
     execute(oldMember: GuildMember, newMember: GuildMember){
-        if(newMember.id == "547459595300503552"){
-            try{
-                newMember.setNickname("boykisser");
-            } catch (e){
-                console.log("failing again");
-            }
+        for(let nameNValue of config.getConfigOption("permanentNicknames").split(",")){
+            if(newMember.id == nameNValue.split(";")[0]){
+                try{
+                    newMember.setNickname(nameNValue.split(";")[1]);
+                } catch (e){
+                    console.log("failing again");
+                }
 
+            }
         }
+
     }
 }
