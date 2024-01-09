2eeaaefb8699bd65ef8c84ea89249cb4b7ce13e1
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-01-09 20:37:20 +0000

    changed name of static folder from 'public' to 'static'

--- app.ts
+++ app.ts
@@ -8,7 +8,7 @@ import {configRouter} from "./router/config";
 export const app = express();
 
 app.use(bodyParser.json());
-app.use(express.static("public"))
+app.use(express.static("static"))
 app.use(logRequest);
 app.use(cors());
 
