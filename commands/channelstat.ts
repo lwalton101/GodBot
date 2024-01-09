2f6587e718a838ee56735ff7e2e73042649ca3b5
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-01-09 20:37:36 +0000

    added channel stat command

--- commands/channelstat.ts
+++ commands/channelstat.ts
@@ -1,5 +1,5 @@
 import {Command} from "../Command";
-import {GuildMember, Message, SlashCommandBuilder} from "discord.js";
+import {Collection, EmbedBuilder, GuildMember, Message, SlashCommandBuilder} from "discord.js";
 export const command: Command = {
     admin: false,
     data: new SlashCommandBuilder()
@@ -9,8 +9,8 @@ export const command: Command = {
         const channel = interaction.channel;
         let fetchID = interaction.id;
         let savedMessages: Message[] = []
+        let userMessageCount: Collection<string, number> = new Collection<string, number>();
         let shouldKeepGoing = true;
-
         await interaction.deferReply();
 
         while (shouldKeepGoing) {
@@ -21,12 +21,32 @@ export const command: Command = {
             }
 
             messages.forEach(message => {
+                if(!userMessageCount.has(message.author.displayName)){
+                    userMessageCount.set(message.author.displayName, 0)
+                }
+
+                var count = userMessageCount.get(message.author.displayName);
+                userMessageCount.set(message.author.displayName, count + 1);
                 savedMessages.push(message);
                 fetchID = message.id;
             });
-
         }
+        userMessageCount.sort();
+        userMessageCount.reverse();
+        var messageCountString = `${savedMessages.length} total messages\n`
+        userMessageCount.forEach((numberOfMessages, userName) => {
+            messageCountString += `${userName}: ${numberOfMessages}\n`
+        })
 
-        await interaction.editReply("done!")
+        var finalEmbed = new EmbedBuilder()
+            .setTitle("Channel Stats")
+            .addFields(
+                {name: "Title:", value:channel.name},
+                {name:"ID:", value:channel.id},
+                {name: "Message Count:", value: messageCountString}
+            ).setColor("Green");
+
+        await interaction.editReply({embeds:[finalEmbed]})
     }
+
 };
