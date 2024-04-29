d191bf80bbd6d09980110e503431c2e692fe95e9
lwalton101 <83783262+lwalton101@users.noreply.github.com>
2024-04-29 19:12:49 +0100

    live updating

--- events/ReadyWordCheck.ts
+++ events/ReadyWordCheck.ts
@@ -1,10 +1,31 @@
 import {EventHandler} from "../EventHandler";
-import {Client, Events} from "discord.js";
+import {Client, Events, Message} from "discord.js";
+import {wordUserRepo} from "../data-source";
 
-export const eventHandler: EventHandler<Events.ClientReady> = {
-    name: Events.ClientReady,
-    once: true,
-    execute(client: Client){
-        console.log(`Ready! Logged in as ${client.user.tag}`);
+export const eventHandler: EventHandler<Events.MessageCreate> = {
+    name: Events.MessageCreate,
+    once: false,
+    async execute(message: Message){
+        const strings = message.content.split(" ");
+        for (let string of strings) {
+            const exists = await wordUserRepo.exists({
+                where: {
+                    word: string,
+                    userId: message.author.id
+                }
+            });
+
+            if(exists){
+                const wordUser = await wordUserRepo.findOneBy({userId: message.author.id, word: string});
+                await wordUserRepo.update({userId: message.author.id, word: string}, {count: wordUser.count + 1})
+            } else{
+                var wordUser = await wordUserRepo.save([{
+                    userId: message.author.id,
+                    word: string,
+                    count: 0
+                }])
+            }
+
+        }
     }
 }
